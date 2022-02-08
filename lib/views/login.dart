46be9f953f212a387c32c8486324260e2494acbd
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2022-02-08 23:03:07 +0530

    Fix some visual issues in forms

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -9,7 +9,7 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Z- Collector")),
+      appBar: AppBar(title: const Text("Z- Collector Login")),
       body: LoginPageForm(),
     );
   }
@@ -17,55 +17,68 @@ class LoginPage extends StatelessWidget {
 
 class LoginPageForm extends StatelessWidget {
   final _loginImage =
-      "https://img.freepik.com/free-vector/isometric-data-protection-concept-with-parent-child-login-window-lock-3d_1284-63713.jpg";
+      "https://imgs.bharatmatrimony.com/bmimgs/login/login-otp-banner.png";
   final _formKey = GlobalKey<FormBuilderState>();
 
   LoginPageForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: FormBuilder(
-        key: _formKey,
-        child: ListView(
-          children: [
-            Image.network(_loginImage),
-            const SizedBox(height: 8),
-            FormBuilderTextField(
-              name: 'email',
-              decoration: const InputDecoration(
-                label: Text('Email Address'),
-                prefixIcon: Icon(Icons.email),
-                border: OutlineInputBorder(),
-              ),
-              validator: FormBuilderValidators.compose([
-                FormBuilderValidators.email(context),
-                FormBuilderValidators.required(context),
-              ]),
+    return FormBuilder(
+      key: _formKey,
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(8),
+              children: [
+                Image.network(_loginImage),
+                FormBuilderTextField(
+                  name: 'email',
+                  decoration: const InputDecoration(
+                    label: Text('Email Address'),
+                    prefixIcon: Icon(Icons.email),
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: FormBuilderValidators.compose(
+                    [
+                      FormBuilderValidators.email(context),
+                      FormBuilderValidators.required(context),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 8),
+                FormBuilderTextField(
+                  name: 'password',
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    label: Text('Password'),
+                    prefixIcon: Icon(Icons.lock),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+              ],
             ),
-            const SizedBox(height: 8),
-            FormBuilderTextField(
-              name: 'password',
-              obscureText: true,
-              decoration: const InputDecoration(
-                label: Text('Password'),
-                prefixIcon: Icon(Icons.lock),
-                border: OutlineInputBorder(),
-              ),
+          ),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _handleSubmit(context),
+                  child: const Text("Submit"),
+                ),
+                TextButton(
+                  onPressed: () => _handleRegister(context),
+                  child: const Text("Don't have an account? Register Now"),
+                ),
+              ],
             ),
-            const SizedBox(height: 8),
-            const Divider(),
-            const SizedBox(height: 8),
-            ElevatedButton(
-              onPressed: () => _handleSubmit(context),
-              child: const Text("Submit"),
-            ),
-            TextButton(
-                onPressed: () => _handleRegister(context),
-                child: const Text("Don't have an account? Register Now"))
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -75,16 +88,14 @@ class LoginPageForm extends StatelessWidget {
   }
 
   void _handleSubmit(BuildContext context) async {
-    final formState = _formKey.currentState;
-    if (formState == null) return;
-
-    final isValid = formState.validate();
-    if (!isValid) return;
+    final formState = _formKey.currentState!;
+    if (!formState.validate()) return;
     formState.save();
 
+    final String email = formState.value['email'];
+    final String password = formState.value['password'];
+
     try {
-      final String email = formState.value['email'];
-      final String password = formState.value['password'];
       await FirebaseAuth.instance
           .signInWithEmailAndPassword(email: email, password: password);
       Navigator.popAndPushNamed(context, '/home');
--- lib/views/register.dart
+++ lib/views/register.dart
@@ -10,88 +10,106 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Z- Collector")),
+      appBar: AppBar(title: const Text("Z- Collector Register")),
       body: RegisterPageForm(),
     );
   }
 }
 
 class RegisterPageForm extends StatelessWidget {
+  final _registerImage = "https://www.dc10g.com/image/login_des.png";
   final _formKey = GlobalKey<FormBuilderState>();
 
   RegisterPageForm({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8),
-      child: FormBuilder(
-        key: _formKey,
-        child: ListView(
-          children: [
-            FormBuilderTextField(
-              name: 'name',
-              decoration: const InputDecoration(
-                label: Text('Name'),
-                prefixIcon: Icon(Icons.account_circle),
-                border: OutlineInputBorder(),
-              ),
-              validator: FormBuilderValidators.required(context),
+    return FormBuilder(
+      key: _formKey,
+      child: Column(
+        children: [
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(8),
+              children: [
+                Image.network(_registerImage),
+                const SizedBox(height: 8),
+                FormBuilderTextField(
+                  name: 'name',
+                  decoration: const InputDecoration(
+                    label: Text('Name'),
+                    prefixIcon: Icon(Icons.account_circle),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: FormBuilderValidators.required(context),
+                ),
+                const SizedBox(height: 8),
+                FormBuilderTextField(
+                  name: 'email',
+                  decoration: const InputDecoration(
+                    label: Text('Email Address'),
+                    prefixIcon: Icon(Icons.email),
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: FormBuilderValidators.compose(
+                    [
+                      FormBuilderValidators.email(context),
+                      FormBuilderValidators.required(context),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 8),
+                FormBuilderTextField(
+                  name: 'password',
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    label: Text('Password'),
+                    prefixIcon: Icon(Icons.lock),
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                const SizedBox(height: 8),
+                FormBuilderTextField(
+                  name: 'retype_password',
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    label: Text('Retype Password'),
+                    prefixIcon: Icon(Icons.lock),
+                    border: OutlineInputBorder(),
+                  ),
+                  validator: _validateRetypePassword,
+                ),
+              ],
             ),
-            const SizedBox(height: 8),
-            FormBuilderTextField(
-              name: 'email',
-              decoration: const InputDecoration(
-                label: Text('Email Address'),
-                prefixIcon: Icon(Icons.email),
-                border: OutlineInputBorder(),
-              ),
-              validator: FormBuilderValidators.compose([
-                FormBuilderValidators.email(context),
-                FormBuilderValidators.required(context),
-              ]),
+          ),
+          const Divider(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                ElevatedButton(
+                  onPressed: () => _handleSubmit(context),
+                  child: const Text("Submit"),
+                ),
+                TextButton(
+                  onPressed: () => _handleLogin(context),
+                  child: const Text("Already have an account? Login Now"),
+                ),
+              ],
             ),
-            const SizedBox(height: 8),
-            FormBuilderTextField(
-              name: 'password',
-              obscureText: true,
-              decoration: const InputDecoration(
-                label: Text('Password'),
-                prefixIcon: Icon(Icons.lock),
-                border: OutlineInputBorder(),
-              ),
-              validator: FormBuilderValidators.required(context),
-            ),
-            const SizedBox(height: 8),
-            FormBuilderTextField(
-              name: 'retype_password',
-              obscureText: true,
-              decoration: const InputDecoration(
-                label: Text('Retype Password'),
-                prefixIcon: Icon(Icons.lock),
-                border: OutlineInputBorder(),
-              ),
-              validator: (value) {
-                if (value != _formKey.currentState?.fields['password']?.value) {
-                  return 'Password and Retype Password do not match';
-                }
-                return null;
-              },
-            ),
-            const SizedBox(height: 8),
-            const Divider(),
-            const SizedBox(height: 8),
-            ElevatedButton(
-              onPressed: () => _handleSubmit(context),
-              child: const Text("Submit"),
-            ),
-            TextButton(
-                onPressed: () => _handleLogin(context),
-                child: const Text("Already have an account? Login Now"))
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+
+  String? _validateRetypePassword(value) {
+    if (value != _formKey.currentState?.fields['password']?.value) {
+      return 'Password and Retype Password do not match';
+    }
+    return null;
   }
 
   void _handleLogin(BuildContext context) {
@@ -99,17 +117,15 @@ class RegisterPageForm extends StatelessWidget {
   }
 
   void _handleSubmit(BuildContext context) async {
-    final formState = _formKey.currentState;
-    if (formState == null) return;
-
-    final isValid = formState.validate();
-    if (!isValid) return;
+    final formState = _formKey.currentState!;
+    if (!formState.validate()) return;
     formState.save();
 
+    final String name = formState.value['name'];
+    final String email = formState.value['email'];
+    final String password = formState.value['password'];
+
     try {
-      final String name = formState.value['name'];
-      final String email = formState.value['email'];
-      final String password = formState.value['password'];
       final credential = await FirebaseAuth.instance
           .createUserWithEmailAndPassword(email: email, password: password);
       await FirebaseFirestore.instance
