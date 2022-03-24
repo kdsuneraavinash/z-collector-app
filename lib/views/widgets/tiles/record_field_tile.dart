f4498aa85f43545a3df5f960cc8d8bb06524b1f2
K. D. Sunera Avinash Chandrasiri <kdsuneraavinash@gmail.com>
2022-03-24 16:37:23 +0530

    Remove unused imports

--- lib/views/widgets/tiles/record_field_tile.dart
+++ lib/views/widgets/tiles/record_field_tile.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:z_collector_app/models/project.dart';
 import 'package:z_collector_app/views/widgets/tiles/boolean_tile.dart';
